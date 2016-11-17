43d1e3d900fed08538e0daead69295f01c05843b
Zac Yauney <zac.yauney@gmail.com>
2016-11-17 12:10:47 -0800

    Idempotent, virtual environments

--- setup.sh
+++ setup.sh
@@ -2,28 +2,35 @@
 # Zac's Ubuntu Setup Script
 
 # Things that are generally important to have
-sudo apt-get install -y curl
-sudo apt-get install pip
+
+# For now these are commented out because they need to be idempotent
+
+#sudo apt-get install -y curl
+#sudo apt-get install pip
+
+cd $HOME
+
+# Install virtual environments
+
+if [ ! -d ./envs/ ]; then
+    git clone https://github.com/Superman8218/envs.git
+fi
 
 # git pull and install dotfiles as well
-cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
+
+if [ ! -d ./dotfiles/ ]; then
+    git clone https://github.com/Superman8218/dotfiles.git
+    ln -sb dotfiles/.screenrc .
+    ln -sb dotfiles/.bash_profile .
+    ln -sb dotfiles/.bashrc .
+    ln -sb dotfiles/.bashrc_custom .
+    ln -sb dotfiles/.bash_aliases .
+    ln -sf dotfiles/.emacs.d .
+    ln -sb dotfiles/.vimrc .
+    ln -sb dotfiles/.tmux.conf .
+    ln -sb dotfiles/.Xresources .
+    ln -sb dotfiles/.Xresources.d .
 fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/Superman8218/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sb dotfiles/.bash_aliases .
-ln -sf dotfiles/.emacs.d .
-ln -sb dotfiles/.vimrc .
-ln -sb dotfiles/.tmux.conf .
-ln -sb dotfiles/.Xresources .
-ln -sb dotfiles/.Xresources.d .
 
 # Install Vundle and .vim folder
 
@@ -32,3 +39,4 @@ if [ ! -d ./.vim/ ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
 fi 
+
