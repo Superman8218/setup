e9722a272b82e6ac2a84fcf85e3b0ca155d4a8a7
Zac Yauney <zac.yauney@gmail.com>
2016-11-21 09:45:41 -0800

    Added push master script

---
+++ push.sh
@@ -0,0 +1,9 @@
+cd ~/dotfiles
+git push origin master
+cd ~/setup
+git push origin master
+cd ~/envs
+git push orgin master
+cd ~/development/emerald
+git push origin master
+echo "All directories have been pushed"
