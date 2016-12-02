627bd04fb70a4a4f5662ce47012f3d5f03226656
Zac Yauney <zac.yauney@gmail.com>
2016-12-02 12:49:23 -0800

    Fixed push script

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ git push origin master
 cd ~/setup
 git push origin master
 cd ~/envs
-git push orgin master
+git push origin master
 cd ~/development/emerald
 git push origin master
 echo "All directories have been pushed"
